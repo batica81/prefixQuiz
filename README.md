1c637dea5e86a6c118c2dc87314b19a9cd24c855
Vojislav Ristivojevic <batica@gmail.com>
2024-05-03 23:36:42 +0200

    First version done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # prefixQuiz
+
+This is a game to help you learn country prefixes.
+It has two modes, guess the country and guess the prefix.
+
+The list was taken from: https://www.itu.int/en/ITU-R/terrestrial/fmd/Pages/call_sign_series.aspx
--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
+
+const allCountries = Object.keys(prefixList);
+const allPrefixes = mergeArrays(prefixList);
+
+let randomCountry =  getRandomKey(prefixList)
+let countryPrefix = getSingleOrRandom(prefixList[randomCountry])
+
 function getRandomKey(obj) {
     const keys = Object.keys(obj);
     const randomIndex = Math.floor(Math.random() * keys.length);
@@ -52,46 +59,107 @@ function nameCleanup(countryName) {
 }
 
 
-const allCountries = Object.keys(prefixList);
-const allPrefixes = mergeArrays(prefixList);
+function initCountries() {
+    let countryOptions = document.getElementsByClassName('countryOptions')[0];
+    countryOptions.innerHTML = '';
 
-let randomCountry =  getRandomKey(prefixList)
-let countryPrefix = getSingleOrRandom(prefixList[randomCountry])
+    let randomCountry =  getRandomKey(prefixList)
+    let countryPrefix = getSingleOrRandom(prefixList[randomCountry])
+    this.innerText =  countryPrefix;
 
-let randomPrefixes = getRandomMembers(allPrefixes, 5, countryPrefix)
-let randomCountries = getRandomMembers(allCountries, 5, randomCountry)
+    // create an array of countries excluding the correct one
+    let randomCountries = getRandomMembers(allCountries, 3, randomCountry)
+
+    randomCountries.forEach(rp => {
+        let tmpDiv = createDivWithTxtAndData(nameCleanup(rp))
+        tmpDiv.addEventListener('click', checkAnswer)
+        countryOptions.appendChild(tmpDiv)
+    })
+
+    // adding correct country to the list
+    let tmpDiv = createDivWithTxtAndData(nameCleanup(randomCountry))
+    tmpDiv.setAttribute("data-param", "correctAnswer");
+    tmpDiv.addEventListener('click', checkAnswer)
+    countryOptions.appendChild(tmpDiv)
+
+    shuffle_children(countryOptions)
+}
 
 
+function initPrefixes() {
+    let prefixOptions = document.getElementsByClassName('prefixOptions')[0];
+    prefixOptions.innerHTML = '';
 
-console.log('countries: ', allCountries)
+    let randomCountry =  getRandomKey(prefixList)
+    this.innerText =  nameCleanup(randomCountry)
 
-console.log('prefixes: ', allPrefixes)
+    // create an array of prefixes excluding the correct one
+    let randomPrefixes = getRandomMembers(allPrefixes, 3, countryPrefix)
 
-console.log('random country: ', nameCleanup(randomCountry))
+    randomPrefixes.forEach(rp => {
+        let tmpDiv = createDivWithTxtAndData(rp)
+        tmpDiv.addEventListener('click', checkAnswer)
+        prefixOptions.appendChild(tmpDiv)
+    })
 
-console.log('one prefix of that country: ', countryPrefix)
+    // adding correct prefix to the list
+    let correctPrefix =  getSingleOrRandom(prefixList[randomCountry])
+    let tmpDiv = createDivWithTxtAndData(correctPrefix)
+    tmpDiv.setAttribute("data-param", "correctAnswer");
+    tmpDiv.addEventListener('click', checkAnswer)
+    prefixOptions.appendChild(tmpDiv)
 
-console.log('random prefixes: ', randomPrefixes);
+    shuffle_children(prefixOptions)
+}
 
-console.log('random countries: ', randomCountries.map(x => nameCleanup(x)));
 
+function createDivWithTxtAndData(textContent) {
+    // Create a new div element
+    let div = document.createElement("div");
+
+    // Set the text content
+    div.textContent = textContent;
+
+    // Optional: Add a class for styling
+    div.classList.add("clickable-div");
+    div.classList.add("answerWrapper");
+
+    return div;
+}
+
+function checkAnswer() {
+    if (this.getAttribute('data-param') === 'correctAnswer') {
+        this.classList.add('correctAnswer')
+    } else {
+        this.classList.add('wrongAnswer')
+    }
+}
+
+function shuffle_children(element) {
+    for (var i = element.children.length; i >= 0; i--) {
+        element.appendChild(element.children[Math.random() * i | 0]);
+    }
+}
 
 window.onload = (event) => {
-
     let gameWrapper = document.getElementsByClassName('gameWrapper')
     let modeChooserButton = document.getElementsByClassName('modeChooserButton')[0];
 
+    let currentPrefix = document.getElementsByClassName('currentPrefix')[0];
+    let currentCountry = document.getElementsByClassName('currentCountry')[0];
+
     let buttonText = 'Guess Prefix'
+
     modeChooserButton.addEventListener('click', function (){
         let currentText = this.innerText;
         this.innerText = buttonText;
         buttonText = currentText;
-            Array.from(gameWrapper).forEach(gw => gw.classList.toggle("hidden"))
-        })
+        Array.from(gameWrapper).forEach(gw => gw.classList.toggle("hidden"))
+    })
 
+    currentPrefix.addEventListener('click', initCountries)
+    currentCountry.addEventListener('click', initPrefixes)
 
+    initPrefixes()
+    initCountries()
 };
-
-
-
-
